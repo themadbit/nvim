fcf8e4d49dee573e51c0c6dfb24922ae6da176ca
themadbit <marktanui75@gmail.com>
2025-12-28 18:07:25 +0300

    chore: clean duplicate copilot configuration

--- lua/kickstart/plugins/copilot.lua
+++ lua/kickstart/plugins/copilot.lua
@@ -1,51 +1,40 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "InsertEnter",
-    config = function()
-      require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
-      })
-    end,
-  },
-  {
-    "CopilotC-Nvim/CopilotChat.nvim",
-    branch = "main",
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'main',
     dependencies = {
-      { "zbirenbaum/copilot.lua" },
-      { "nvim-lua/plenary.nvim" },
+      { 'zbirenbaum/copilot.lua' },
+      { 'nvim-lua/plenary.nvim' },
     },
     opts = {
       debug = false,
       -- Your preferred mappings
       mappings = {
         complete = {
-          detail = "Use @<Tab> or /<Tab> for options.",
-          insert = "<Tab>",
+          detail = 'Use @<Tab> or /<Tab> for options.',
+          insert = '<Tab>',
         },
         close = {
-          normal = "q",
-          insert = "<C-c>",
+          normal = 'q',
+          insert = '<C-c>',
         },
         reset = {
-          normal = "<C-r>",
-          insert = "<C-r>",
+          normal = '<C-r>',
+          insert = '<C-r>',
         },
         submit_prompt = {
-          normal = "<CR>",
-          insert = "<C-s>",
+          normal = '<CR>',
+          insert = '<C-s>',
         },
       },
     },
     keys = {
-      { "<leader>cc", ":CopilotChatToggle<CR>", desc = "Toggle Copilot Chat" },
-      { "<leader>ce", ":CopilotChatExplain<CR>", desc = "Explain code" },
-      { "<leader>ct", ":CopilotChatTests<CR>", desc = "Generate tests" },
-      { "<leader>cf", ":CopilotChatFix<CR>", desc = "Fix code" },
-      { "<leader>co", ":CopilotChatOptimize<CR>", desc = "Optimize code" },
-      { "<leader>cd", ":CopilotChatDocs<CR>", desc = "Generate docs" },
+      { '<leader>cc', ':CopilotChatToggle<CR>', desc = 'Toggle Copilot Chat' },
+      { '<leader>ce', ':CopilotChatExplain<CR>', desc = 'Explain code' },
+      { '<leader>ct', ':CopilotChatTests<CR>', desc = 'Generate tests' },
+      { '<leader>cf', ':CopilotChatFix<CR>', desc = 'Fix code' },
+      { '<leader>co', ':CopilotChatOptimize<CR>', desc = 'Optimize code' },
+      { '<leader>cd', ':CopilotChatDocs<CR>', desc = 'Generate docs' },
     },
   },
 }
